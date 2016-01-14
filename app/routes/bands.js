4998a0093c404bf51b137cfbb9aa17ed67e06aef
Tyler Newman <tylerjnewman18@gmail.com>
2016-01-13 16:21:57 -0800

    import Ember from ember fix bug

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,18 +1,20 @@
 import Ember from 'ember';
+import Band from '../models/band';
+import Song from '../models/song';
 
-var Band = Ember.Object.extend({
-  name: '',
+// var Band = Ember.Object.extend({
+//   name: '',
 
-  slug: Ember.computed('name', function() {
-    return this.get('name').dasherize();
-  })
-});
+//   slug: Ember.computed('name', function() {
+//     return this.get('name').dasherize();
+//   })
+// });
 
-var Song = Ember.Object.extend({
-  title: '',
-  rating: 0,
-  band: ''
-});
+// var Song = Ember.Object.extend({
+//   title: '',
+//   rating: 0,
+//   band: ''
+// });
 
 var blackDog = Song.create({
   title: 'Black Dog',
